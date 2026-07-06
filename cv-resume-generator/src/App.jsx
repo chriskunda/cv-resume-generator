dfb3c03d7b2169223aa1323a6946c308e6124b3e
chriskunda <christianira2003@gmail.com>
2026-07-06 11:34:14 +0200

    ,,,

---
+++ cv-resume-generator/src/App.jsx
@@ -0,0 +1,12 @@
+import PersonalInfo from './components/PersonalInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My CV</h1>
+      <PersonalInfo />
+    </div>
+  );
+}
+
+export default App;
